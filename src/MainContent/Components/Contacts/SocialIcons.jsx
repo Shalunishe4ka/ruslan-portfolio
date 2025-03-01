9060b7c94c175c63cb5176a3a7c3181fb4167868
Shalunishe4ka <gt05dag@mail.ru>
2025-03-02 00:40:36 +0300

    ссылки на страницы

--- src/MainContent/Components/Contacts/SocialIcons.jsx
+++ src/MainContent/Components/Contacts/SocialIcons.jsx
@@ -8,18 +8,18 @@ export const SocialIcons = () => {
     return (
         <div className='socialIcons-container'>
             <div className='socialIcons-items'>
-            <div className='socialIcons-info'>
-                <img src={tgIcon} alt='tgIcon' />
-                <p>@jesuisdetesteparlemondeentierrr</p>
-            </div>
-            <div className='socialIcons-info'>
-                <img src={vkIcon} alt='vkIcon' />
-                <p>@jesuisdetesteparlemondeentierrr</p>
-            </div>
-            <div className='socialIcons-info'>
-                <img src={googleIcon} alt='googleIcon' />
-                <p>fleurfeel7@gmail.com</p>
-            </div>
+                <div className='socialIcons-info'>
+                    <img src={tgIcon} alt='tgIcon' />
+                    <a href='https://t.me/jesuisdetesteparlemondeentierrr'>@jesuisdetesteparlemondeentierrr</a>
+                </div>
+                <div className='socialIcons-info'>
+                    <img src={vkIcon} alt='vkIcon' />
+                    <a href='https://vk.com/jesuisdetesteparlemondeentierrr'>@jesuisdetesteparlemondeentierrr</a>
+                </div>
+                <div className='socialIcons-info'>
+                    <img src={googleIcon} alt='googleIcon' />
+                    <a href="mailto:fleurfeel7@gmail.com">fleurfeel7@gmail.com</a>
+                </div>
             </div>
         </div>
     )
--- src/MainContent/MainContent.jsx
+++ src/MainContent/MainContent.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { WhoIAm } from './Components/WhoIAm/WhoIAm'
-import { AboutMe } from './Components/WhoIAm/AboutMe/AboutMe'
+// import { AboutMe } from './Components/WhoIAm/AboutMe/AboutMe'
 import { Education } from './Components/Education/Education'
 import { Skills } from './Components/Skills/Skills'
 import { Contacts } from './Components/Contacts/Contacts'
